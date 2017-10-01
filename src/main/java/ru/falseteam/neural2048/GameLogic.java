8fe934cf453d41f5f6b6b10e4ddb7db5dcfdf5d1
Prog <prog32bit@ya.ru>
2017-10-01 23:01:48 +0300

    Complete logic and some refactoring

--- src/main/java/ru/falseteam/neural2048/GameLogic.java
+++ src/main/java/ru/falseteam/neural2048/GameLogic.java
@@ -47,13 +47,11 @@ public class GameLogic extends GameData {
             case UP:
             case DOWN:
                 for (int i = 0; i < 4; ++i) {
-                    int[] r = Arrays.copyOf(theGrid[i], theGrid.length);
-                    if (direction.equals(Directions.DOWN)) ArrayUtils.reverse(r);
-                    Shifted s = shift(r);
-                    if (s.changed) {
-                        if (direction.equals(Directions.DOWN)) ArrayUtils.reverse(s.row);
-                        theGrid[i] = Arrays.copyOf(s.row, r.length);
-                        score += s.score;
+                    int[] row = Arrays.copyOf(theGrid[i], theGrid.length);
+                    if (direction.equals(Directions.DOWN)) ArrayUtils.reverse(row);
+                    if (shift(row)) {
+                        if (direction.equals(Directions.DOWN)) ArrayUtils.reverse(row);
+                        theGrid[i] = Arrays.copyOf(row, row.length);
                         flag = false;
                     }
                 }
@@ -61,14 +59,13 @@ public class GameLogic extends GameData {
             case LEFT:
             case RIGHT:
                 for (int i = 0; i < 4; ++i) {
-                    int[] r = new int[theGrid.length];
-                    for (int k = 0; k < r.length; ++k) r[k] = theGrid[k][i];
-                    if (direction.equals(Directions.DOWN) || direction.equals(Directions.RIGHT)) ArrayUtils.reverse(r);
-                    Shifted s = shift(r);
-                    if (s.changed) {
-                        if (direction.equals(Directions.RIGHT)) ArrayUtils.reverse(s.row);
-                        for (int k = 0; k < r.length; ++k) theGrid[k][i] = s.row[k];
-                        score += s.score;
+                    int[] row = new int[theGrid.length];
+                    for (int k = 0; k < row.length; ++k) row[k] = theGrid[k][i];
+                    if (direction.equals(Directions.DOWN) || direction.equals(Directions.RIGHT))
+                        ArrayUtils.reverse(row);
+                    if (shift(row)) {
+                        if (direction.equals(Directions.RIGHT)) ArrayUtils.reverse(row);
+                        for (int k = 0; k < row.length; ++k) theGrid[k][i] = row[k];
                         flag = false;
                     }
                 }
@@ -78,45 +75,24 @@ public class GameLogic extends GameData {
         screen.redraw(this);
     }
 
-    private class Shifted {
-        boolean changed = false;
-        int[] row = new int[4];
-        int score = 0;
-
-        void change() {
-            changed = true;
-        }
-
-        Shifted(int length) {
-            this.row = new int[length];
-        }
-    }
-
-    // TODO: 01.10.17 пофиксить сдвиг
-    private Shifted shift(int[] row) {
-        Shifted result = new Shifted(row.length);
-
+    private boolean shift(int[] row) {
+        int[] reRow = new int[row.length];
         for (int i = 0, j = 1, k = 0; k < row.length; ) {
-            if (j > row.length - 1) {
-                result.row[k++] = i < row.length && row[i] != 0 ? row[i++] : 0;
-            } else if (row[i] == 0) {
-                j = ++i + 1;
-            } else if (row[j] == 0) {
-                ++j;
-            } else if (row[i] == row[j++]) {
-                result.row[k] = row[i] + 1;
-                result.score += 1 << result.row[k];
-                if (result.row[k] > maxTileExp) maxTileExp = result.row[k];
+            if (j > row.length - 1) reRow[k++] = i < row.length && row[i] != 0 ? row[i++] : 0;
+            else if (row[i] == 0) j = ++i + 1;
+            else if (row[j] == 0) ++j;
+            else if (row[i] == row[j++]) {
+                reRow[k] = row[i] + 1;
+                score += 1 << reRow[k];
+                if (reRow[k++] > maxTileExp) maxTileExp = reRow[k - 1];
                 i = j++;
-                ++k;
-                result.change();
             } else {
-                result.row[k++] = row[i];
+                reRow[k++] = row[i];
                 i = j - 1;
             }
         }
-        result.changed = true;
-
-        return result;
+        if (Arrays.equals(reRow, row)) return false;
+        System.arraycopy(reRow, 0, row, 0, reRow.length);
+        return true;
     }
 }
