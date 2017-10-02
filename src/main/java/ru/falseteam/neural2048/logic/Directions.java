2995374626b7f0ad0fe037ed75ccb792c7963904
Vladislav <vladislavsumin@mail.ru>
2017-10-02 10:55:09 +0300

    add comment add boolean to move return

--- src/main/java/ru/falseteam/neural2048/logic/Directions.java
+++ src/main/java/ru/falseteam/neural2048/logic/Directions.java
@@ -1,5 +1,10 @@
 package ru.falseteam.neural2048.logic;
 
+/**
+ * Перечесление возможных ходов
+ *
+ * @version 1.0
+ */
 public enum Directions {
     UP, DOWN, LEFT, RIGHT
 }
--- src/main/java/ru/falseteam/neural2048/logic/GameData.java
+++ src/main/java/ru/falseteam/neural2048/logic/GameData.java
@@ -1,8 +1,13 @@
 package ru.falseteam.neural2048.logic;
 
+/**
+ * Полный набор данных, описывающих текущее состояние игры
+ *
+ * @version 1.0
+ */
 public class GameData {
     public int theGrid[][] = new int[4][4];
     public int score = 0;
     public GameState state;
-    public int maxTileExp = 1;
+    public int maxTileExp = 1; // Плитка максимального размера
 }
