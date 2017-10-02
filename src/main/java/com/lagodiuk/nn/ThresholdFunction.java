a62e9859aeafce94d0d05f994bfd0576902b4503
Vladislav <vladislavsumin@mail.ru>
2017-10-02 21:00:54 +0300

    fix thresholdFunction

--- src/main/java/com/lagodiuk/nn/ThresholdFunction.java
+++ src/main/java/com/lagodiuk/nn/ThresholdFunction.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright 2012 Yuriy Lagodiuk
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
 package com.lagodiuk.nn;
 
 import javax.xml.bind.annotation.XmlEnum;
@@ -20,86 +5,81 @@ import javax.xml.bind.annotation.XmlEnumValue;
 import javax.xml.bind.annotation.XmlType;
 import java.util.*;
 
+/**
+ * Перечисление содержащие набор функций-активаторов
+ *
+ * @version 1.0
+ */
 @XmlType(name = "basic-threshold-functions")
 @XmlEnum
 public enum ThresholdFunction {
 
-	@XmlEnumValue("LINEAR")
-	LINEAR {
-		@Override
-		public double calculate(double value, List<Double> params) {
-			double a = params.get(0);
-			double b = params.get(1);
-			return (a * value) + b;
-		};
+    @XmlEnumValue("LINEAR")
+    LINEAR {
+        @Override
+        public double calculate(double value, List<Double> params) {
+            double a = params.get(0);
+            double b = params.get(1);
+            return (a * value) + b;
+        }
 
-		@Override
-		public List<Double> getDefaultParams() {
-			double a = 1;
-			double b = 0;
-			List<Double> ret = new LinkedList<Double>();
-			ret.add(a);
-			ret.add(b);
-			return ret;
-		}
-	},
-	@XmlEnumValue("SIGN")
-	SIGN {
-		@Override
-		public double calculate(double value, List<Double> params) {
-			double threshold = params.get(0);
-			if (value > threshold) {
-				return 1;
-			} else {
-				return 0;
-			}
-		};
+        @Override
+        public List<Double> getDefaultParams() {
+            List<Double> ret = new LinkedList<>();
+            ret.add(1d);
+            ret.add(0d);
+            return ret;
+        }
+    },
 
-		@Override
-		public List<Double> getDefaultParams() {
-			double threshold = 0;
-			List<Double> ret = new LinkedList<Double>();
-			ret.add(threshold);
-			return ret;
-		}
-	},
-	@XmlEnumValue("SIGMA")
-	SIGMA {
-		@Override
-		public double calculate(double value, List<Double> params) {
-			double a = params.get(0);
-			double b = params.get(1);
-			double c = params.get(2);
-			return a / (b + Math.expm1(-value * c) + 1);
-		}
+    @XmlEnumValue("SIGN")
+    SIGN {
+        @Override
+        public double calculate(double value, List<Double> params) {
+            double threshold = params.get(0);
+            if (value > threshold) return 1;
+            return 0;
+        }
 
-		@Override
-		public List<Double> getDefaultParams() {
-			double a = 1;
-			double b = 1;
-			double c = 1;
-			List<Double> ret = new ArrayList<Double>(3);
-			ret.add(a);
-			ret.add(b);
-			ret.add(c);
-			return ret;
-		}
-	};
+        @Override
+        public List<Double> getDefaultParams() {
+            List<Double> ret = new LinkedList<Double>();
+            ret.add(0d);
+            return ret;
+        }
+    },
 
-	private static final Random random = new Random();
+    @XmlEnumValue("SIGMA")
+    SIGMA {
+        @Override
+        public double calculate(double value, List<Double> params) {
+            double a = params.get(0);
+            double b = params.get(1);
+            double c = params.get(2);
+            return a / (b + Math.expm1(-value * c) + 1);
+        }
 
-	public static ThresholdFunction getRandomFunction() {
-		ThresholdFunction[] allFunctions = values();
-		return allFunctions[random.nextInt(allFunctions.length)];
-	}
+        @Override
+        public List<Double> getDefaultParams() {
+            double a = 1;
+            double b = 1;
+            double c = 1;
+            List<Double> ret = new ArrayList<Double>(3);
+            ret.add(a);
+            ret.add(b);
+            ret.add(c);
+            return ret;
+        }
+    };
 
-	public double calculate(double value, List<Double> params) {
-		// Stub
-		return 0;
-	}
+    private static final Random random = new Random();
 
-	public List<Double> getDefaultParams() {
-		// Stub
-		return Collections.emptyList();
-	}
+    public static ThresholdFunction getRandomFunction() {
+        ThresholdFunction[] allFunctions = values();
+        return allFunctions[random.nextInt(allFunctions.length)];
+    }
+
+    public abstract double calculate(double value, List<Double> params);
+
+    public abstract List<Double> getDefaultParams();
 }
