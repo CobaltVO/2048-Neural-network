b5f0ba7620aa68806a4f92a7575a7e34c453226a
vladislavsumin <vladislavsumin@mail.ru>
2017-10-02 20:15:47 +0300

    add network creator

--- src/main/java/ru/falseteam/neural2048/players/RandomNeuralNetwork.java
+++ src/main/java/ru/falseteam/neural2048/players/RandomNeuralNetwork.java
@@ -9,6 +9,7 @@ import com.lagodiuk.nn.genetic.OptimizableNeuralNetwork;
 import ru.falseteam.neural2048.logic.Directions;
 import ru.falseteam.neural2048.logic.GameLogic;
 import ru.falseteam.neural2048.logic.GameState;
+import ru.falseteam.neural2048.nn.NetworkCreator;
 
 import java.util.Random;
 
@@ -17,7 +18,8 @@ public class RandomNeuralNetwork {
     public RandomNeuralNetwork(GameLogic gameLogic) {
         new Thread(() -> {
             Population<OptimizableNeuralNetwork> population = new Population<>();
-            OptimizableNeuralNetwork nn = initNeuralNetwork();
+//            OptimizableNeuralNetwork nn = initNeuralNetwork();
+            OptimizableNeuralNetwork nn = NetworkCreator.initNeuralNetwork(16, 64, 32, 16, 2);
 
             for (int i = 0; i < 80; i++) {
                 population.addChromosome(nn.mutate());
@@ -29,8 +31,8 @@ public class RandomNeuralNetwork {
                         nn.putSignalToNeuron(i, gameLogic.theGrid[i / 4][i % 4]);
                     }
                     nn.activate();
-                    int i = nn.getAfterActivationSignal(80) > 0.5 ? 0b01 : 0b00;
-                    i |= nn.getAfterActivationSignal(81) > 0.5 ? 0b10 : 0b00;
+                    int i = nn.getAfterActivationSignal(nn.getNeuronsCount() - 1) > 0.5 ? 0b01 : 0b00;
+                    i |= nn.getAfterActivationSignal(nn.getNeuronsCount() - 2) > 0.5 ? 0b10 : 0b00;
                     while (!gameLogic.move(Directions.values()[i])) {
                         i++;
                         i &= 3;
@@ -45,7 +47,7 @@ public class RandomNeuralNetwork {
             GeneticAlgorithm<OptimizableNeuralNetwork, Double> env =
                     new GeneticAlgorithm<OptimizableNeuralNetwork, Double>(population, fit);
 
-            //private Random random = new Random();
+            final Random random = new Random();
             env.addIterationListener(environment -> {
                 OptimizableNeuralNetwork gene = environment.getBest();
                 Double d = environment.fitness(gene);
@@ -54,8 +56,8 @@ public class RandomNeuralNetwork {
 //                    if (d <= 0.1) {
 //                        environment.terminate();
 //                    }
-//                    environment.setParentChromosomesSurviveCount(this.random.nextInt(environment.getPopulation().getSize()));
-                environment.setParentChromosomesSurviveCount(20);
+                //environment.setParentChromosomesSurviveCount(random.nextInt(environment.getPopulation().getSize()));
+                environment.setParentChromosomesSurviveCount(40);
                 //environment.evolve(10);
             });
 
