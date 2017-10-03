dca7fb2ea73fca434328d5f42f2c51187362fa04
vladislavsumin <vladislavsumin@mail.ru>
2017-10-03 19:19:34 +0300

    1

--- src/main/java/ru/falseteam/neural2048/Statistics.java
+++ src/main/java/ru/falseteam/neural2048/Statistics.java
@@ -9,8 +9,11 @@ import ru.falseteam.neural2048.players.Player;
 public class Statistics {
     private final GameLogic gameLogic;
     private final Player player;
+
     private long scopeSum = 0;
     private long gameCount = 0;
+    private int lowest = Integer.MAX_VALUE;
+    private int highest = 0;
 
     public Statistics(GameLogic gameLogic, Player player) {
         this.gameLogic = gameLogic;
@@ -21,10 +24,23 @@ public class Statistics {
         gameLogic.restart();
         for (int i = 0; i < count; i++) {
             player.playOneGame(gameLogic);
-            scopeSum += gameLogic.score;
+            int score = gameLogic.score;
+            scopeSum += score;
             gameCount++;
+
+            if (lowest > score) lowest = score;
+            if (highest < score) highest = score;
+
             gameLogic.restart();
         }
+    }
+
+    public void printStatistics() {
+        System.out.println("Player name: " + player.getClass().getSimpleName());
+        System.out.println("Game count: " + gameCount);
         System.out.println("Average score: " + (scopeSum / gameCount));
+        System.out.println("Lowest score: " + lowest);
+        System.out.println("Highest score: " + highest);
+
     }
 }
