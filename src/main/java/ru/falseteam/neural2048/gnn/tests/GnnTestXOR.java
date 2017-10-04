ac20c66f9079f74849e95bfa9b81a3d2caab689a
vladislavsumin <vladislavsumin@mail.ru>
2017-10-04 18:45:51 +0300

    optimize test

--- src/main/java/ru/falseteam/neural2048/gnn/tests/GnnTestXOR.java
+++ src/main/java/ru/falseteam/neural2048/gnn/tests/GnnTestXOR.java
@@ -13,15 +13,16 @@ import java.util.List;
 import java.util.Random;
 
 public class GnnTestXOR {
-    private static final int[] NETWORK_CONFIG = {2, 1, 1};
+    private static final int[] NETWORK_CONFIG = {2, 1};
     private static final ThresholdFunction[] thresholdFunctions = {
             ThresholdFunction.SIGMA
     };
     private static final int POPULATION_SIZE = 20;
-    private static final int ITERATION = 70;
 
     private static final Random RANDOM = new Random();
 
+    private static final int a[] = {0, 0, 1, 1};
+    private static final int b[] = {0, 1, 0, 1};
 
     public static void main(String[] args) {
         //Создаем случайную нейронную сеть с задаными параметрами
@@ -42,18 +43,16 @@ public class GnnTestXOR {
 
         Fitness<GeneticNeuralNetwork, Integer> fit = chromosome -> {
             int score = 0;
-            for (int i = 0; i < ITERATION; i++) {
-                int a = RANDOM.nextInt(2);
-                int b = RANDOM.nextInt(2);
-                chromosome.putSignalToNeuron(0, a);
-                chromosome.putSignalToNeuron(1, b);
+            for (int i = 0; i < 4; i++) {
+                chromosome.putSignalToNeuron(0, a[i]);
+                chromosome.putSignalToNeuron(1, b[i]);
 
                 chromosome.activate();
 
                 int c = ((int) chromosome.getAfterActivationSignal(chromosome.getNeuronsCount() - 1));
-                if (c == (a ^ b)) score++;
+                if (c == (a[i] ^ b[i])) score++;
             }
-            return ITERATION - score;
+            return 4 - score;
         };
 
         GeneticAlgorithm<GeneticNeuralNetwork, Integer> env =
@@ -75,19 +74,13 @@ public class GnnTestXOR {
         env.evolve(100000);
 
         nn = env.getBest();
-        int a[] = {0, 0, 1, 1};
-        int b[] = {0, 1, 0, 1};
 
         for (int i = 0; i < 4; i++) {
             nn.putSignalToNeuron(0, a[i]);
             nn.putSignalToNeuron(1, b[i]);
-
             nn.activate();
-
             int c = ((int) nn.getAfterActivationSignal(nn.getNeuronsCount() - 1));
-
             if (c != (a[i] ^ b[i])) throw new RuntimeException();
-
             System.out.println(a[i] + " ^ " + b[i] + " = " + c);
         }
     }
